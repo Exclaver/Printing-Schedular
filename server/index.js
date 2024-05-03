015e27c4337c73cd054ef0a7355c61ff4081b420
Exclaver <138944787+Exclaver@users.noreply.github.com>
2024-05-03 23:07:47 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -24,7 +24,7 @@ app.use(cors(
 ));
 app.use(express.json());
 app.get("/", (req, res) => {
-  res.send("Hello, Express!");
+  res.send("Hello, Expressssssssssss!");
 });
 app.post('/api/delete', deleteFiles);
 
