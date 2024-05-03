0645a99f16bc6975857e8ea612e07419af46fa2e
Exclaver <138944787+Exclaver@users.noreply.github.com>
2024-05-03 22:52:20 +0530

    Update config.js

--- server/config.js
+++ server/config.js
@@ -2,7 +2,7 @@ const baseDir = __dirname;
 
 const Config = {
   PORT: 5000,
-  BACKEND_DOMAIN: "https://printing-schedular-ndclmrvoj-devanshs-projects-32554901.vercel.app//api",
+  BACKEND_DOMAIN: "https://printing-schedular-ndclmrvoj-devanshs-projects-32554901.vercel.app/api",
   BASE_DIR: baseDir,
   FILE_LINK_EXPIRY_IN_MINUTES: 1, // i am setting 1 minute expiry time to check this functionality working or not
   CRYPTR_TOKEN: "fddffd",
